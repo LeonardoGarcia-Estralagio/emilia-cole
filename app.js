472647ef966717357cb9bf23ba64c05f6ba8b5e7
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-06-27 13:37:11 -0300

    feat: agrega archivos html y sus rutas

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const PORT = 3030;
 
 app.use(expres.static("public"));
 
+/* Rutas */
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "about.html"))
+);
+app.get("/music", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "music.html"))
+);
+app.get("/contact", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "contact.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
